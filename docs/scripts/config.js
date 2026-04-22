3b1852556cc9d74bc5049549fec2885b456da1ce
matmodelator <druzball@tutamail.com>
2026-04-23 00:47:38 +0300

    Update config.js

--- docs/scripts/config.js
+++ docs/scripts/config.js
@@ -7,10 +7,10 @@ from "https://esm.sh/@supabase/supabase-js@2"
 // ======================
 
 export const SUPABASE_URL =
-  "https://fvkcqttqkglzpozftvbg.supabase.co"
+  'https://fvkcqttqkglzpozftvbg.supabase.co'
 
 export const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2a2NxdHRxa2dsenBvemZ0dmJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4NjY5MTAsImV4cCI6MjA5MjQ0MjkxMH0.S3E8TIa_4ktdrD9dTBqmIJRyu3E6EN2lnnpzdUEbR2U"
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2a2NxdHRxa2dsenBvemZ0dmJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4NjY5MTAsImV4cCI6MjA5MjQ0MjkxMH0.S3E8TIa_4ktdrD9dTBqmIJRyu3E6EN2lnnpzdUEbR2U'
 
 // ======================
 // Client
